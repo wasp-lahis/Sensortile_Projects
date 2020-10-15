0e53594ba86d233104c6ab1752740315dc6df41d
Lahis Gomes de Almeida <lahis.almeida@eldorado.org.br>
2020-10-14 21:09:50 -0300

    update python codes

--- Bluetooth_Communication/Scripts/Raspberry Pi/ble_interfaces.py
+++ Bluetooth_Communication/Scripts/Raspberry Pi/ble_interfaces.py
@@ -1,10 +1,36 @@
 #!/usr/bin/env python
 
-# DESCRIPTION
-#
-# This application example shows how to perform a Bluetooth Low Energy (BLE)
-# scan, connect to a device, retrieve its exported features, and get push
-# notifications from it.
+################################################################################
+# COPYRIGHT(c) 2018 STMicroelectronics                                         #
+#                                                                              #
+# Redistribution and use in source and binary forms, with or without           #
+# modification, are permitted provided that the following conditions are met:  #
+#   1. Redistributions of source code must retain the above copyright notice,  #
+#      this list of conditions and the following disclaimer.                   #
+#   2. Redistributions in binary form must reproduce the above copyright       #
+#      notice, this list of conditions and the following disclaimer in the     #
+#      documentation and/or other materials provided with the distribution.    #
+#   3. Neither the name of STMicroelectronics nor the names of its             #
+#      contributors may be used to endorse or promote products derived from    #
+#      this software without specific prior written permission.                #
+#                                                                              #
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"  #
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE    #
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE   #
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE    #
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR          #
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF         #
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS     #
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN      #
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)      #
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE   #
+# POSSIBILITY OF SUCH DAMAGE.                                                  #
+################################################################################
+
+################################################################################
+# Author:  Davide Aliprandi, STMicroelectronics                                #
+################################################################################
+
 
 
 # IMPORT
@@ -14,7 +40,6 @@ import sys
 import os
 import time
 from abc import abstractmethod
-
 
 from blue_st_sdk.manager import ManagerListener
 from blue_st_sdk.node import NodeListener
@@ -87,6 +112,7 @@ class MyNodeListener(NodeListener):
 class MyFeatureListener(FeatureListener):
 
     _notifications = 0
+    _acc_coordinates = [] # [x,y,z]
     """Counting notifications to print only the desired ones."""
 
     #
@@ -96,6 +122,11 @@ class MyFeatureListener(FeatureListener):
     # @param sample  Data extracted from the feature.
     #
     def on_update(self, feature, sample):
-        print(feature)
+        if feature.get_name() == "Accelerometer":
+            x_axis = feature.get_accelerometer_x(sample)
+            y_axis = feature.get_accelerometer_y(sample)
+            z_axis = feature.get_accelerometer_z(sample)
+            
+            self._acc_coordinates = [x_axis, y_axis, z_axis]
 
 
